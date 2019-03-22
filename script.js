f694f6b3dba83cfcccc7d8ec0f0116897397bb77
Alex Maday <alex.maday@gmail.com>
2019-03-21 21:20:49 -0500

    add global object to hold boxShadow attributes

--- script.js
+++ script.js
@@ -4,15 +4,30 @@ document.addEventListener("DOMContentLoaded", function() {
   //     return document.getElementById(id).value;
   //   }
 
+  let boxShadowAttribute = {
+    x: "0",
+    y: "0",
+    blur: "0",
+    spread: "0",
+    getAttributes: function() {
+      let rangeValues = `${this.x}, ${this.y}, ${this.blur}, ${this.spread}`;
+      console.log(rangeValues);
+
+      return rangeValues;
+    }
+  };
+
   function changeBoxShadow() {
     let box = document.getElementById("box");
-    box.style.boxShadow = xOffset.value;
+    box.style.boxShadow = boxShadowAttribute.getAttributes;
   }
 
   document.getElementById("x").addEventListener("input", function() {
     // update label values
     let rangeValue = document.getElementsByClassName("label-range-value")[0];
     rangeValue.textContent = this.value;
+    boxShadowAttribute.x = this.value;
+    changeBoxShadow();
   });
   document.getElementById("y").addEventListener("input", function() {
     let rangeValue = document.getElementsByClassName("label-range-value")[1];
